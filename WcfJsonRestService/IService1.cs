fd7bb7c77c30d34d48f8b646ff705312e9896fba
Mecena1703 <uholaza73@gmail.com>
2015-01-11 19:46:46 +0100

    full

---
+++ WcfJsonRestService/IService1.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace WcfJsonRestService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+      
+        [OperationContract]
+        Guitar GetGuitarData(string id);
+
+        [OperationContract]
+        Guitar[] GetGuitars(string id);
+    }
+}
---
+++ WcfJsonRestService/Service1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel.Web;
+
+namespace WcfJsonRestService
+{
+    public class Service1 : IService1
+    {
+      
+        [WebInvoke(Method = "GET",
+                  ResponseFormat = WebMessageFormat.Json,
+                  UriTemplate = "guitar/{id}")]
+        public Guitar GetGuitarData(string id)
+        {
+            return new Guitar(id);
+        }
+         [WebInvoke(Method = "GET",
+                  ResponseFormat = WebMessageFormat.Json,
+                  UriTemplate = "guitars/{id}")]
+        public Guitar[] GetGuitars(string id)
+        {
+            if (!string.IsNullOrEmpty(id) && id!="0")
+                return new Guitar[] { new Guitar(id) };
+            
+            return new Guitar[] {
+                new Guitar("1"),
+                new Guitar("2"),
+                new Guitar("3")
+            };
+        }
+    }
+  
+    [DataContract]
+    public class Guitar
+    {
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Image { get; set; }
+
+        public Guitar(string id)
+        {
+            switch (id)
+            {
+                case "1":
+                    Name = "Akustara";
+                    Description = "Akustična gitara marke ŠBRK";
+                    Image = "Acoustic";
+                    break;
+                case "2":
+                    Name = "Električna";
+                    Description = "Električna gitara marke ŠBRK";
+                    Image = "Electric";
+                    break;
+                case "3":
+                    Name = "Bas";
+                    Description = "Bas gitara marke ŠBRK";
+                    Image = "Bass";
+                    break;
+
+            }
+        }
+    }
+}
